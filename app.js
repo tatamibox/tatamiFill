8707368e696a63927cb98fb7975772bfb8963313
tatamibox <tat4mi@gmail.com>
2022-06-28 20:46:11 -0700

    webstorage api mechanism

---
+++ app.js
@@ -0,0 +1,7 @@
+const index = require('./index');
+
+
+
+if (window.location.href.includes('https://docs.google.com/forms/')) {
+    hurt();
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+
+const loadProfileButton = document.querySelector('.loadProfileButton');
+
+//form selectors
+const discordField = document.querySelector('#discord');
+const emailField = document.querySelector('#email');
+const passwordField = document.querySelector('#password');
+const submitNewProfileButton = document.querySelector('.submitNewProfile');
+//
+
+// profile constructor
+const profiles = [chrome.storage.sync.get('Profiles')];
+class Profile {
+    constructor(email, discord, password) {
+        this.email = email;
+        this.discord = discord;
+        this.password = password;
+    }
+}
+//
+
+// set fields to chrome storage sync
+submitNewProfileButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const profile = new Profile(emailField.value, discord.value, passwordField.value);
+    profiles.push(profile);
+    chrome.storage.sync.set({ Profiles: profiles });
+    chrome.storage.sync.get('Profiles');
+})
+//
+
+
+
